49a2252cb91939b7f2923ceafa61921e7dedcc90
Henrik___ <Henrik.Wenck@student.hpi.de>
2019-11-14 11:34:02 +0100

    removed not used packages and added comments and markdown

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -3,20 +3,15 @@ package de.hpi.ddm.actors;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.Arrays;
-
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.serialization.*;
 import akka.actor.Props;
-import akka.util.MessageBuffer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import sun.rmi.rmic.Util;
 
 import java.lang.Math;
 
@@ -44,6 +39,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private ActorRef receiver;
 	}
 
+	//two ints were added
 	@Data @NoArgsConstructor @AllArgsConstructor
 	public static class BytesMessage<T> implements Serializable {
 		private static final long serialVersionUID = 4057807743872319842L;
@@ -82,13 +78,10 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
+
+		// Solution option:
 		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
+
 		
 		// Serialize the object with the akka.serializable library
 		Serialization serialization = SerializationExtension.get(this.getContext().getSystem());
@@ -97,23 +90,22 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		int serializerID = serialization.findSerializerFor(message.getMessage()).identifier();
 		String manifest = Serializers.manifestFor(serialization.findSerializerFor(message.getMessage()), message.getMessage());
 
-
+		//Chunk byte array into array chunk of length 8096
 		int distance = 8096;
 		int endcount = (int) Math.ceil((double) bytes.length/distance);
-		//System.out.println(bytes.length + "HAALAOOFDFDS");
-		// chunk size to divide
 		for(int messagecount=0;messagecount<endcount;messagecount+=1){
 			byte[] byte_chunk= Arrays.copyOfRange(bytes, messagecount*distance, Math.min(bytes.length,(messagecount+1)*distance));
 			receiverProxy.tell(new BytesMessage<byte[]>(byte_chunk, manifest, serializerID, this.sender(), message.getReceiver(),messagecount,endcount), this.self());
 		}
-		System.out.println("VORBEI!!!!!");
-
-		//receiverProxy.tell(new BytesMessage<>(bytes, manifest, serializerID, this.sender(), message.getReceiver()), this.self());
 	}
 
 	private void handle(BytesMessage<?> message) {
 		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-		System.out.println("1. "+ message.messagecount + "2. " + message.endcount);
+
+		// ******* should be deleted before delivery ********
+		System.out.println("1. "+ message.messagecount + "    2. " + message.endcount);
+
+		//collects the array chunks and brings them together
 		if(message.messagecount<message.endcount){
 			byte[] test = (byte[]) message.bytes;
 			try {
@@ -123,6 +115,8 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 				e.printStackTrace();
 			}
 		}
+
+		//whole message will be deserialized
 		if(message.messagecount==(message.endcount)-1) {
 			Serialization serialization = SerializationExtension.get(this.getContext().getSystem());
 			Object mes = serialization.deserialize(messageBuffer.toByteArray(), message.serializerID, message.manifest).get();
@@ -132,6 +126,3 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		}
 	}
 }
-
-//TODO: Include ByteBuffer ?
-//https://doc.akka.io/docs/akka/current/remoting-artery.html#bytebuffer-based-serialization
